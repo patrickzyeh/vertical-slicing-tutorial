bbf0d36b4b51535a8ad014ad404570329f90ba85
patrickzyeh <patrickzyeh@gmail.com>
2025-01-17 11:14:24 -0500

    Added support for multiple items (close #4)

--- mcmerch.py
+++ mcmerch.py
@@ -3,14 +3,16 @@ import argparse
 def main():
     #configure first cli arg to be the price of the item 
     parser = argparse.ArgumentParser()
+    parser.add_argument("quantity", type=int, help="The quantity of the item")
     parser.add_argument("price", type=float, help="The price of the item")
     args = parser.parse_args()
-    price = args.price
+    price = args.quantity * args.price
 
     # add tax to the price
     total = price * 1.13 # Since in Ontario
 
-    print(f"The totla price is {total: .2f}")
+    print(f"The total price is {total: .2f}")
+
 
 if __name__ == '__main__':
     main()
